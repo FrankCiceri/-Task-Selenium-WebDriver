6c01419a4f6ac61e03f5577ef853448cd1ee1a83
FrankCiceri <12fa1.fciceri@gmail.com>
2025-01-29 17:10:15 -0500

    Added method for argument check

--- ExtraClass/ArgumentChecker.cs
+++ ExtraClass/ArgumentChecker.cs
@@ -10,7 +10,10 @@ namespace webdriver_task_3.ExtraClass
     {
         public static void CheckArgumentIsNull<T>(T argument, string argumentName)
         {
-            if (argument is null) throw new ArgumentNullException(argumentName, $"{argumentName} Can't Be Null");
+            if (argument is null)
+            {
+                throw new ArgumentNullException(argumentName, $"{argumentName} Can't Be Null");
+            }
         }
     }
 }
