0661faf64dacd2de465b97433741e31d78a2df2d
FrankCiceri <frankanthonyc00@gmail.com>
2025-02-03 21:54:46 -0500

    Added Git Ignore to Business

--- Selenium WebDriver.Business/obj/Debug/net8.0/Selenium WebDriver.Business.AssemblyInfo.cs
+++ Selenium WebDriver.Business/obj/Debug/net8.0/Selenium WebDriver.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Selenium WebDriver.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba02747284bef38d8bb75a8e19f447fcc13a5aab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e510e8449e734b41d7d2b7213fd8b36d977a5e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Selenium WebDriver.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Selenium WebDriver.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
